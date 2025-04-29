74b73f37d60273928e44b1f6c6fb3cef206ad2de
Furkan Altıntaş <furkanaltintas785@gmail.com>
2025-04-30 00:16:13 +0300

    Routes

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,8 +1,12 @@
 import { Routes } from '@angular/router';
 import { HomeComponent } from './components/home/home.component';
+import { FoodPageComponent } from './components/food-page/food-page.component';
+import { CartPageComponent } from './components/cart-page/cart-page.component';
 
 export const routes: Routes = [
     { path: '', component: HomeComponent},
     { path: 'search/:searchTerm', component: HomeComponent},
-    { path: 'tag/:tag', component: HomeComponent }
+    { path: 'tag/:tag', component: HomeComponent },
+    { path: 'food/:id', component: FoodPageComponent},
+    { path: 'cart-page', component: CartPageComponent }
 ];
